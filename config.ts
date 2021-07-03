551253a1f59d8f56662a786a239acbc5b95dfae2
Vozdyx <vozdyx.dev@gmail.com>
2021-07-03 16:55:59 +0300

    Update config.ts
    Исправлен тип токена

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export const config = Object({ 
     "socket": "wss://socket.donationalerts.ru",
     "socket-port": "443",
-    "type": "minor",
+    "type": "alert_widget",
 
     "donation-token": "",
     "pick": "https://cdn.discordapp.com/attachments/732914762698194995/769910239196741662/J2MT9A04_400x400.png",
